85fd982bbc13b167d9a57d72e9d813d96ba905f1
Abdulhaseeb 033 <ahaseebans110@gmail.com>
2026-08-20 17:09:32 +0500

    configure backend for vercel

--- Backend/server.js
+++ Backend/server.js
@@ -11,14 +11,10 @@ app.use(express.json());
 
 app.use("/api/products", productRoutes);
 
-const PORT = process.env.PORT || 3000;
-
 app.get("/", (req, res) => {
     res.send("SHOP.CO Backend is running...");
 });
 
 connectDB();
 
-app.listen(PORT, () => {
-    console.log(`Server is running on ${PORT}`);
-});
+export default app;
