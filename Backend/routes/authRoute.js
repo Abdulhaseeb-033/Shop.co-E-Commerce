1dc4e573f1e34c5d0c19d47b815b9c27cec45a39
Abdulhaseeb 033 <ahaseebans110@gmail.com>
2026-09-09 19:16:11 +0500

    Add JWT authentication middleware

--- Backend/routes/authRoute.js
+++ Backend/routes/authRoute.js
@@ -1,8 +1,15 @@
 import express from "express";
 import { signup, login } from "../controllers/authController.js";
+import authMiddleware from "../middleware/authmiddleware.js";
 
 const router = express.Router();
 router.post("/signup", signup);
 router.post("/login", login);
+router.get("/protected", authMiddleware, (req, res) => {
+    res.status(200).json({
+        message: "You are authenticated",
+        user: req.user
+    });
+})
 
 export default router;
