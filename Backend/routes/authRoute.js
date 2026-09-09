1dedcae333f1d185c4a8d8d8013ac66bf130e0bb
Abdulhaseeb 033 <ahaseebans110@gmail.com>
2026-09-09 19:37:06 +0500

    temparary route deleted

--- Backend/routes/authRoute.js
+++ Backend/routes/authRoute.js
@@ -5,11 +5,5 @@ import authMiddleware from "../middleware/authmiddleware.js";
 const router = express.Router();
 router.post("/signup", signup);
 router.post("/login", login);
-router.get("/protected", authMiddleware, (req, res) => {
-    res.status(200).json({
-        message: "You are authenticated",
-        user: req.user
-    });
-})
 
 export default router;
