8180f5f8d74d28bf34f771cfb77457717c355af8
Abdulhaseeb 033 <ahaseebans110@gmail.com>
2026-09-09 19:45:33 +0500

    Add admin role middleware

--- Backend/routes/authRoute.js
+++ Backend/routes/authRoute.js
@@ -1,9 +1,16 @@
 import express from "express";
 import { signup, login } from "../controllers/authController.js";
 import authMiddleware from "../middleware/authmiddleware.js";
+import adminMiddleware from "../middleware/adminmiddleware.js";
 
 const router = express.Router();
 router.post("/signup", signup);
 router.post("/login", login);
+router.get("/admin-test", authMiddleware, adminMiddleware, (req, res) => {
+    res.json({ 
+        message: "Welcome Admin",
+        user: req.user
+    });
+});
 
 export default router;
