5d00dd39a47445910c728f2ee29085f7d42720f8
Abdulhaseeb 033 <ahaseebans110@gmail.com>
2026-09-10 03:47:52 +0500

    Order APIs complete

--- Backend/controllers/orderController.js
+++ Backend/controllers/orderController.js
@@ -76,3 +76,34 @@ export const getOrderById = async (req, res) => {
         });
     } 
 };
+
+export const updateOrderStatus = async (req, res) => {
+    try {
+        const { status } = req.body;
+
+        const order = await Order.findByIdAndUpdate(
+            req.params.id,
+            { status },
+            {
+                new: true,
+                runValidators: true
+            }
+        );
+
+        if (!order) {
+            return res.status(404).json({
+                message: "Order not found"
+            });
+        }
+
+        res.status(200).json({
+            message: "Order status updated successfully",
+            order
+        });
+    } catch (error) {
+        res.status(500).json({
+            message: "Failed to update order satus",
+            error: error.message
+        });
+    }
+};
--- Backend/routes/orderRoute.js
+++ Backend/routes/orderRoute.js
@@ -1,6 +1,6 @@
 import express from "express";
 
-import { createOrder, getOrders, getOrderById } from "../controllers/orderController.js";
+import { createOrder, getOrders, getOrderById, updateOrderStatus } from "../controllers/orderController.js";
 import authMiddleware from "../middleware/authmiddleware.js";
 import adminMiddleware from "../middleware/adminmiddleware.js";
 
@@ -9,4 +9,5 @@ const router = express.Router();
 router.post("/", authMiddleware, createOrder);
 router.get("/", authMiddleware, adminMiddleware, getOrders);
 router.get("/:id", authMiddleware, adminMiddleware, getOrderById);
+router.patch("/:id", authMiddleware, adminMiddleware, updateOrderStatus);
 export default router;
