893cebeb2e79abe48d0d117d609831aad0e7d7d4
Abdulhaseeb 033 <ahaseebans110@gmail.com>
2026-09-09 14:43:28 +0500

    Add product update and delete APIs

--- Backend/controllers/productController.js
+++ Backend/controllers/productController.js
@@ -5,7 +5,7 @@ export const createProduct = async (req, res) => {
         const product = await Product.create(req.body);
 
         res.status(201).json({
-            mesage: "Product created successfully.",
+            message: "Product created successfully.",
             product
         });
     } catch (error) {
@@ -43,12 +43,63 @@ export const getProductById = async (req, res) => {
         }
 
         res.status(200).json({
-            message: "Product fetched seccessfully",
+            message: "Product fetched successfully",
             product
         });
     } catch (error) {
-        res,status(500).json({
+        res.status(500).json({
             message:"Failed to fetch product",
+            error: error.message
+        });
+    }
+};
+
+export const updateProduct = async (req, res) => {
+    try {
+        const product =await Product.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            {
+                new: true,
+                runValidators: true
+            }
+        );
+
+        if(!product) {
+            return res.status(404).json({
+                message: "Product not found"
+            });
+        }
+
+        res.status(200).json({
+            message: "Product updated successfully",
+            product
+        });
+    } catch (error) {
+        res.status(500).json({
+            message: "Failed to update product",
+            error: error.message
+        });
+    }
+};
+
+export const deleteProduct =async (req, res) => {
+    try {
+        const product = await Product.findByIdAndDelete(req.params.id);
+
+        if(!product) {
+            return res.status(404).json({
+                message: "Product not found"
+            });
+        }
+
+        res.status(200).json({
+            message: "Product deleted successfully",
+            product
+        });
+    } catch (error) {
+        res.status(500).json({
+            message: "Failed to delete product",
             error: error.message
         });
     }
