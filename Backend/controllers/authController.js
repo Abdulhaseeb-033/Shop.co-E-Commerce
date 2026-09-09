be9d9f2fed9bd7114c7b9f3a1878c426bea31e2a
Abdulhaseeb 033 <ahaseebans110@gmail.com>
2026-09-09 18:53:45 +0500

    Add user login Auhtentication

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -1,5 +1,6 @@
 import User from "../models/User.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res) => {
     try {
@@ -38,3 +39,50 @@ export const signup = async (req, res) => {
         });
     }
 };
+
+export const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        if (!email || !password) {
+            return res.status(400).json({ message: "All fields are required" });
+        }
+
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ message: "Invalid credentials" });
+        }
+
+        const isPasswordCorrect = await bcrypt.compare(password, user.password);
+        if (!isPasswordCorrect) {
+            return res.status(400).json({ message: "Invalid credentials" });
+        }
+
+        const token = jwt.sign(
+            { 
+                id: user._id, 
+                role: user.role
+            },
+            process.env.JWT_SECRET,
+            { 
+                expiresIn: "7d"
+            }
+        );
+
+        res.status(200).json({
+            message: "Login successful",
+            token,
+            user: {
+                id: user._id,
+                name: user.name,
+                email: user.email,
+                role: user.role
+            }
+        });
+    } catch (error) {
+        res.status(500).json({ 
+            message: "Error logging in",
+            error: error.message
+        });
+    }
+}
