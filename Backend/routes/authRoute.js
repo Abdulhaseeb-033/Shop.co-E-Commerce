0837c2ac9f465265199cbf0eb0209f857892b160
Abdulhaseeb 033 <ahaseebans110@gmail.com>
2026-09-10 03:02:35 +0500

    Fix order schema and auth routes

--- Backend/routes/authRoute.js
+++ Backend/routes/authRoute.js
@@ -1,7 +1,5 @@
 import express from "express";
 import { signup, login } from "../controllers/authController.js";
-import authMiddleware from "../middleware/authmiddleware.js";
-import adminMiddleware from "../middleware/adminmiddleware.js";
 
 const router = express.Router();
 router.post("/signup", signup);
